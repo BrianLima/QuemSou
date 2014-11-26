a27ea985e265a2376a8f5592b9e13d5c2623a7d6
Brian Lima <Brian.ghwt@gmail.com>
2014-11-26 20:53:23 -0200

    Finished gameplayability. Bug fixes.
    Fixed bug when minimizing the aplication and sorting a word again
    without restarting the clock.
    Added base for Ads.
    Finished gameplay page.
    Interface fixes.

--- QuemSou/GamePage.xaml.cs
+++ QuemSou/GamePage.xaml.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Devices.Sensors;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using System;
+using System.Device.Location;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Navigation;
 using System.Windows.Threading;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Microsoft.Devices.Sensors;
-using Microsoft.Xna.Framework;
-using System.Threading;
 
 namespace QuemSou
 {
-    public partial class GamePage : PhoneApplicationPage
+    public partial class GamePage : PhoneApplicationPage, IDisposable
     {
         private Player _player;
         private Game _game;
         private readonly DispatcherTimer _interfaceTimer;
         private bool _started, _playing;
         private int _seconds;
+        private GeoCoordinateWatcher gcw = null;
 
         public GamePage()
         {
             InitializeComponent();
             
             TitlePanel.Visibility = Visibility.Collapsed;
-            ContentPanel.Visibility = Visibility.Collapsed;
-            
+
+            //Initiating the GeoCordinater and assigning it's handlers
+            this.gcw = new GeoCoordinateWatcher();
+            this.gcw.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(gcw_PositionChanged);
+            this.gcw.Start();
+
+
             var accelerometer = new Accelerometer {TimeBetweenUpdates = TimeSpan.FromMilliseconds(1000)};
             accelerometer.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(accelerometer_CurrentValueChanged);
             accelerometer.Start();
@@ -52,20 +52,27 @@ namespace QuemSou
             }
             else if (_playing)
             {
-                var text = TimeSpan.FromSeconds((_seconds));
-                this.Dispatcher.BeginInvoke((Action)(() => ClockBlock.Text = text.Minutes + ":" + text.Seconds));
+                var time = TimeSpan.FromSeconds((_seconds));
+                string minutes = time.Minutes.ToString();
+                string seconds = time.Seconds.ToString();
+
+                if (seconds.Length == 1)
+                {
+                    seconds = "0" + seconds;
+                }
+
+                this.Dispatcher.BeginInvoke((Action)(() => ClockBlock.Text = minutes + ":" + seconds));
                 _seconds--;
             }
             
-            if (_seconds == 0 && ! _playing)
+            if (_seconds < 0 && ! _playing)
             {
                 this.Dispatcher.BeginInvoke((Action)(() => { InstructionsPanel.Visibility = Visibility.Collapsed; }));
-                this.Dispatcher.BeginInvoke((Action)(() => { ContentPanel.Visibility = Visibility.Visible; }));
                 this.Dispatcher.BeginInvoke((Action)(() => { TitlePanel.Visibility = Visibility.Visible; }));
                 _seconds = 299;
                 _playing = true;
             }
-            else if (_seconds == 0 && _playing)
+            else if (_seconds < 0 && _playing)
             {
                 MessageBox.Show("Fim de jogo");
                 _interfaceTimer.Stop();
@@ -86,20 +93,72 @@ namespace QuemSou
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            string category = NavigationContext.QueryString["category"];
-            _player = new Player(category);
-            _game = new Game();
+            if (_player == null)
+            {
+                string category = NavigationContext.QueryString["category"];
+                _player = new Player(category);
+                _game = new Game();
 
-            UpdateCurrentWord();
+                UpdateCurrentWord();
 
-            LayoutRoot.DataContext = _player;
+                LayoutRoot.DataContext = _player;
 
-            base.OnNavigatedTo(e);
+                base.OnNavigatedTo(e);
+            }
         }
 
         void UpdateCurrentWord()
         {
             _player.setCurrentWord(_game.Play(_player.category));
         }
+
+        private void gcw_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            // Stop the GeoCoordinateWatcher now that we have the device location.
+            this.gcw.Stop();
+
+            AdControl.Latitude = e.Position.Location.Latitude;
+            AdControl.Longitude = e.Position.Location.Longitude;
+
+            Debug.WriteLine("Device lat/long: " + e.Position.Location.Latitude + ", " + e.Position.Location.Longitude);
+        }
+
+        private void adControl1_ErrorOccurred(object sender, Microsoft.Advertising.AdErrorEventArgs e)
+        {
+            Debug.WriteLine("AdControl error: " + e.Error.Message);
+        }
+
+
+        private void AdControl_AdRefreshed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("AdControl new ad received");
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.gcw != null)
+                {
+                    this.gcw.Dispose();
+                    this.gcw = null;
+                }
+
+                if (this._interfaceTimer.IsEnabled)
+                {
+                    this._interfaceTimer.Stop();
+                }
+            }
+        }
+
+        #endregion
     }
 }
